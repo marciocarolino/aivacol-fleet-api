ecc1a40fd3a0fa6288e39e774f17671fd0223ff7
Marcio Carolino <marciocarolino@gmail.com>
2026-06-03 07:13:28 -0300

    feat: add user domain contracts

---
+++ src/app/domain/users/entities/user.entity.ts
@@ -0,0 +1,9 @@
+export class UserEntity {
+  constructor(
+    public readonly id: string,
+    public readonly nickname: string,
+    public readonly name: string,
+    public readonly email: string,
+    public readonly password: string,
+  ) {}
+}
